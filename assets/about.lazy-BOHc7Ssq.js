e2cc431ca45c2288afb3ca22e2736b9c12337c50
samir0ps <samir0ps@users.noreply.github.com>
2024-07-12 05:10:51 +0000

    deploy: a78da09039bc6c03949b06411189b8a1c9a72bf2

---
+++ assets/about.lazy-BOHc7Ssq.js
@@ -0,0 +1,1 @@
+import{c as o,j as t}from"./index-CENdafGY.js";const a=o("/about")({component:()=>t.jsx("div",{children:"Hello /about!"})});export{a as Route};
---
+++ assets/index.lazy-CuyPbNyu.js
@@ -0,0 +1,1 @@
+import{c as e,j as o}from"./index-CENdafGY.js";const r=e("/")({component:()=>o.jsx("div",{children:"Hello /!"})});export{r as Route};
